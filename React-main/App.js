97aad760018acd9118f442e41cc2efa59b253b3b
Arthur Silva <arthur.silva707@gmail.com>
2022-11-17 17:28:06 -0300

    Enviando códigos

---
+++ React-main/App.js
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <img src={require('./assets/sekiro.jpg')} 
+      style={{ width: 420, height: 420 }}/>
+      <Text>Arthur Silva.</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
